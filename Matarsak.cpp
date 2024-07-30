bf834d8d355348f0f107ab00745815dbe2cb4541
Your Name <m.m.soleimani84@gmail.com>
2024-07-30 06:37:57 +0330

    Overloading and overriding several functions and adding comments to the Matarsak class definition and implementation file

--- Matarsak.cpp
+++ Matarsak.cpp
@@ -1,28 +1,37 @@
 #include "Matarsak.hpp"
 #include "Card.hpp"
 #include "UI.hpp"
+#include "Player.hpp"
 
-// A constructor to initialize and create an instance of the Matarsak class
+// constructor to initialize and create an instance of the Matarsak class
 Matarsak::Matarsak() : Card::Card("matarsak", 0, "When a player plays a matarsak, he can be one of his soldiers (yellow cards).\ntake it from the ranks of his army and put it back in his hand.\ntip:\nSpecial (purple) cards cannot be returned to the hand with matarsak.\n", Color::Purple) {}
 
-// Implementation of the function of returning the card to the user
-bool Matarsak::perform_Action(std::vector<Card *> &playerCard)
+// Returning the card to the user
+std::string Matarsak::perform_Action(Player &player)
 {
     UI ui;
-    ui.Display_Message("Enter a card to return it to the card deck :");
+    ui.Display_Message("Enter a card to return it to your hand: ");
     std::string choice = ui.Get_User_Input<std::string>();
 
-    Card *temp_ptr;
-    for (size_t i = 0; i < playerCard.size(); i++)
-    {
-        if (playerCard[i]->Get_Type() == choice)
-        {
-            // temp_ptr = playerCard[i];
-            // playerCard.erase(playerCard.begin() + i);
-            // break;
+    std::vector<Card *> &played_cards = player.Get_played_crads(); // Get a reference to the vector
 
-            // TO_DO : Implementation of the logic of returning the card to the user
+    // Find the card with the specified type
+    for (size_t i = 0; i < played_cards.size(); i++)
+    {
+        if (played_cards[i]->Get_Type() == choice)
+        {
+            // Found the card, move it back to the player's hand
+            player.Add_Card(played_cards[i]);
+            played_cards.erase(played_cards.begin() + i); // Erase the card from the vector
+            return Get_Type() + " played";
         }
     }
-    return true;
+    ui.Display_Message("Card not found in played cards !");
+    return "Card not found in played cards !";
 }
+
+// To handle the absence of errors due to overload and override
+std::string Matarsak::perform_Action(std::vector<Card *> &playerCard) {}
+
+// To handle the absence of errors due to overload and override
+std::string Matarsak::perform_Action() {}
--- Matarsak.hpp
+++ Matarsak.hpp
@@ -5,8 +5,10 @@
 class Matarsak : public Card
 {
 public:
-    Matarsak(); // // A constructor to initialize and create an instance of the Matarsak class
-    virtual bool perform_Action(std::vector<Card *> &) override;    // Implementation of the function of returning the card to the user
+    Matarsak();                                                         // constructor to initialize and create an instance of the Matarsak class
+    virtual std::string perform_Action(Player &) override;              // Override and overloaded function to apply card abilities to the game ; in this cas , Returning the card to the user
+    virtual std::string perform_Action() override;                      // To handle the absence of errors due to overload and override
+    virtual std::string perform_Action(std::vector<Card *> &) override; // To handle the absence of errors due to overload and override
 };
 
 #endif
