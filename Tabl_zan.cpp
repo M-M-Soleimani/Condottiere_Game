1d0432c0732f29675e06dd09992dc141b0d4ec68
Your Name <m.m.soleimani84@gmail.com>
2024-07-30 06:33:28 +0330

    Overloading and overriding several functions and adding comments to the Tabl_zan class definition and implementation file

--- Tabl_zan.cpp
+++ Tabl_zan.cpp
@@ -1,14 +1,16 @@
 #include "Card.hpp"
 #include "Tabl_zan.hpp"
+#include "Player.hpp"
 
 // Constructor to create an instance of class Tabl_zan
-Tabl_zan::Tabl_zan() 
-: Card::Card("tabl_zan" ,0 ,"When a player plays the female drummer, the total number of his soldiers (yellow) is doubled.\nFor example, a player who has a 2nd soldier and a 6th soldier in his army ranks with the game\nDrumming changes the total number of his army from 8 to 16.\ntip:\nOnly one Drummer card affects each player's army per battle.\nMore than one female drum can be played, but it has no effect.\n" ,Color::Purple ){}
+Tabl_zan::Tabl_zan()
+    : Card::Card("tabl_zan", 0, "When a player plays the female drummer, the total number of his soldiers (yellow) is doubled.\nFor example, a player who has a 2nd soldier and a 6th soldier in his army ranks with the game\nDrumming changes the total number of his army from 8 to 16.\ntip:\nOnly one Drummer card affects each player's army per battle.\nMore than one female drum can be played, but it has no effect.\n", Color::Purple) {}
 
-//  An overridden function to double the value of all cards played by the user
-bool Tabl_zan::perform_Action(std::vector<Card *> &playerCard)
+// An overridden and overloaded function to duplicate the value of all cards played by the user
+std::string Tabl_zan::perform_Action(Player &Player)
 {
-    for (Card *card : playerCard)
+    std::vector<Card *> &played_Card = Player.Get_played_crads();
+    for (Card *card : played_Card)
     {
         if (card->Get_Color() == Card::Color::Yellow)
         {
@@ -16,5 +18,11 @@ bool Tabl_zan::perform_Action(std::vector<Card *> &playerCard)
             card->Set_Score(new_score);
         }
     }
-    return true;
+    return Get_Type() + " played";
 }
+
+// To handle the absence of errors due to overload and override
+std::string Tabl_zan::perform_Action(std::vector<Card *> &playerCard) {}
+
+// To handle the absence of errors due to overload and override
+std::string Tabl_zan::perform_Action() {}
--- Tabl_zan.hpp
+++ Tabl_zan.hpp
@@ -5,8 +5,10 @@
 class Tabl_zan : public Card
 {
 public:
-    Tabl_zan(); // Constructor to create an instance of class Tabl_zan
-    virtual bool perform_Action(std::vector<Card *> &) override;    //  An overridden function to double the value of all cards played by the user
+    Tabl_zan();                                                         // Constructor to create an instance of class Tabl_zan
+    virtual std::string perform_Action() override;                      // To handle the absence of errors due to overload and override
+    virtual std::string perform_Action(Player &) override;              // An overridden and overloaded function to duplicate the value of all cards played by the user
+    virtual std::string perform_Action(std::vector<Card *> &) override; // To handle the absence of errors due to overload and override
 };
 
 #endif
