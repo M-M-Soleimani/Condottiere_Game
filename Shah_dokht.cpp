5a282c58a4bdba966580a1ab6e772947668684e1
Your Name <m.m.soleimani84@gmail.com>
2024-08-09 20:55:54 +0330

    set score of Shah_dokht card

--- Shah_dokht.cpp
+++ Shah_dokht.cpp
@@ -2,7 +2,10 @@
 #include "Shah_dokht.hpp"
 
 // Constructor to create an instance of class Shah_dokht
-Shah_dokht::Shah_dokht() : Card("shah_dokht", 10, "Each shah_dokht card increases the strength of the player's army by 10 units.\ntip:\nRisfid, matarsak, zemestan, bahar and tabl_zan do not affect the shah_dokht.\n", Color::Purple) {}
+Shah_dokht::Shah_dokht() : Card("shah_dokht", 10, "Each shah_dokht card increases the strength of the player's army by 10 units.\ntip:\nRisfid, matarsak, zemestan, bahar and tabl_zan do not affect the shah_dokht.\n", Color::Purple)
+{
+    this->Set_Score(10);
+}
 
 // Override and overloaded function to apply card abilities to the game ; in this case, no special feature is applied
 std::string Shah_dokht::perform_Action()
