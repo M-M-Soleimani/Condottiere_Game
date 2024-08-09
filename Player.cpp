d44bcd2ca295a114bdc2363d5b621d91ec5b9878
Your Name <m.m.soleimani84@gmail.com>
2024-08-09 19:10:34 +0330

    Adding new functions and new data members to the player class
    New functions: Get_War_Badge - Set_War_Badge - Get_Score - Set_Score - burning_Hand - burning_Played_Cards

--- Player.cpp
+++ Player.cpp
@@ -5,33 +5,28 @@
 Player::Player(const std::string &name, const int &age, const std::string &color) : name(name), age(age), color(color), acquired_provinces(0) {};
 
 // class destructor to clear the card stack to prevent memory leaks
-Player::~Player()
-{
-    for (Card *card : hand)
-    {
-        delete card;
-    }
-}
+Player::~Player() {}
 
 // Add card to player hand
-void Player::Add_Card(Card *const card)
+void Player::Add_Card(std::shared_ptr<Card> card)
 {
     hand.push_back(card);
 }
 
 // Play card with player
-bool Player::Play_Card(const std::string &cardType)
+std::shared_ptr<Card> Player::Play_Card(const std::string &cardType)
 {
+    std::shared_ptr<Card> temp = nullptr;
     for (auto it = hand.begin(); it != hand.end(); ++it)
     {
         if ((*it)->Get_Type() == cardType)
         {
             played_crads.push_back(*it);
             hand.erase(it);
-            return true;
+            break;
         }
     }
-    return false;
+    return temp;
 }
 
 // Return name of player
@@ -53,47 +48,76 @@ std::string Player::Get_Color() const
 }
 
 // Return Acquired Provinces of player
-int Player::Get_Acquired_Provinces() const
+std::vector<std::string> Player::Get_Acquired_Provinces() const
 {
     return acquired_provinces;
 }
 
+void Player::Add_Acquired_Provinces(const std::string &new_province)
+{
+    acquired_provinces.push_back(new_province);
+}
+
 // Set the hand of player
-void Player::Set_hand(const std::vector<Card *> &cards)
+void Player::Set_hand(const std::vector<std::shared_ptr<Card>> &cards)
 {
     hand = cards;
 }
 
 // Return hand of player
-std::vector<Card *> &Player::Get_hand()
+std::vector<std::shared_ptr<Card>> Player::Get_hand()
 {
     return hand;
 }
 
 // Set the played cards
-void Player::Set_played_crads(const std::vector<Card *> &cards)
+void Player::Set_played_crads(const std::vector<std::shared_ptr<Card>> &cards)
 {
     played_crads = cards;
 }
 
 // Return cards that player played
-std::vector<Card *> &Player::Get_played_crads()
+std::vector<std::shared_ptr<Card>> &Player::Get_played_crads()
 {
     return played_crads;
 }
 
-// Display hand of player
-void Player::display_hand() const
+// Returns whether the player has a war badge or not
+bool Player::Get_War_Badge()
 {
-    UI ui;
-    ui.Display_Message(this->Get_Name());
-    ui.Display_Message("'s hand : ");
-    bool is_first = true;
-    for (Card const *const card : hand)
+    return war_badge;
+}
+
+// Set player have war badge or not
+void Player::Set_War_Badge(const bool &war_badge)
+{
+    this->war_badge = war_badge;
+}
+
+// Return card score
+int Player::Get_Score() const
+{
+    return score;
+}
+
+// Set the value of the score with the input parameter
+void Player::Set_Score(const int &score)
+{
+    this->score = score;
+}
+
+// Clear cards in hand of player
+void Player::burning_Hand(std::vector<std::shared_ptr<Card>> & played_cards)
+{
+    for(auto card : hand)
     {
-        if (!is_first)
-            ui.Display_Message(" - ");
-        ui.Display_Message(card->Get_Type());
-        is_first = false;
+        played_cards.push_back(card);
     }
+    hand.clear();
+}
+
+// Clear played card of player
+void Player::burning_Played_Cards()
+{
+    played_crads.clear();
 }
--- Player.hpp
+++ Player.hpp
@@ -7,27 +7,35 @@
 class Player
 {
 public:
-    Player(const std::string &, const int &, const std::string &); // constructor to initialize and create an instance of the player class
-    ~Player();                                                     // Destructor for player class
-    void Add_Card(Card *const);                                    // Adding cards to the player's hand
-    bool Play_Card(const std::string &);                           // The function of playing cards by the player
-    std::string Get_Name() const;                                  // Return name of player
-    int Get_Age() const;                                           // Return age of player
-    std::string Get_Color() const;                                 // Return color of player
-    void Set_hand(const std::vector<Card *> &);                    // set the hand of player
-    std::vector<Card *> &Get_hand();                               // Return hand of player
-    void Set_played_crads(const std::vector<Card *> &);            // set the played cards
-    std::vector<Card *> &Get_played_crads();                       // Return cards that player played
-    int Get_Acquired_Provinces() const;                            // Return Acquired Provinces of player
-    void display_hand() const;                                     // display hand of player
+    Player(const std::string &, const int &, const std::string &);     // constructor to initialize and create an instance of the player class
+    ~Player();                                                         // Destructor for player class
+    void Add_Card(std::shared_ptr<Card>);                              // Adding cards to the player's hand
+    std::shared_ptr<Card> Play_Card(const std::string &);              // The function of playing cards by the player
+    std::string Get_Name() const;                                      // Return name of player
+    int Get_Age() const;                                               // Return age of player
+    std::string Get_Color() const;                                     // Return color of player
+    void Set_hand(const std::vector<std::shared_ptr<Card>> &);         // set the hand of player
+    std::vector<std::shared_ptr<Card>> Get_hand();                     // Return hand of player
+    void Set_played_crads(const std::vector<std::shared_ptr<Card>> &); // set the played cards
+    std::vector<std::shared_ptr<Card>> &Get_played_crads();            // Return cards that player played
+    std::vector<std::string> Get_Acquired_Provinces() const;           // Return Acquired Provinces of player
+    void Add_Acquired_Provinces(const std::string &);                  // Adding the name of the conquered province to the conquered provinces vector
+    bool Get_War_Badge();                                              // Returns whether the player has a war badge or not
+    void Set_War_Badge(const bool &);                                  // Set player have war badge or not
+    int Get_Score() const;                                             // Return card score
+    void Set_Score(const int &);                                       // Set the value of the score with the input parameter
+    void burning_Hand(std::vector<std::shared_ptr<Card>> &);           // Clear cards in hand of player
+    void burning_Played_Cards();                                       // Clear played card of player
 
 private:
     std::string name;
     int age;
     std::string color;
-    std::vector<Card *> hand;
-    std::vector<Card *> played_crads;
-    int acquired_provinces;
+    std::vector<std::shared_ptr<Card>> hand;
+    std::vector<std::shared_ptr<Card>> played_crads;
+    std::vector<std::string> acquired_provinces;
+    bool war_badge = false;
+    int score = 0;
 };
 
 #endif
