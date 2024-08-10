019cfbfb9333d50744d0f2f7a1c4f94fdbc859d4
Your Name <m.m.soleimani84@gmail.com>
2024-08-10 16:47:05 +0330

    Adding comments to class Heroine

--- Heroine.cpp
+++ Heroine.cpp
@@ -1,6 +1,6 @@
 #include "Heroine.hpp"
 
-// Constructor to create an instance of the yellow card class with parameter
+// Constructor to create an instance of the Heroine class
 Heroine::Heroine() : Card("heroine", 1, "heroine :\nIt has two features :\n1. Each heroine card increases the strength of the player's army by 1 unit.\n2. At the end of the war, the player who has played the most heroine cards determines the location of the next battle. If the players are equal in the number of heroine cards, the same winning player determines the location of the next battle.", Card::Color::Purple)
 {
     this->Set_Score(1);
--- Heroine.hpp
+++ Heroine.hpp
@@ -8,7 +8,7 @@
 class Heroine : public Card
 {
 public:
-    Heroine();                                                          // Constructor to create an instance of the yellow card class with parameter
+    Heroine();                                                          // Constructor to create an instance of the Heroine class 
     std::string perform_Action();                                       // Override and overloaded function to apply card abilities to the game
     std::string perform_Action(std::shared_ptr<Player> &);              // To handle the absence of errors due to overload and override
     std::string perform_Action(std::vector<std::shared_ptr<Player>> &); // To handle the absence of errors due to overload and override
