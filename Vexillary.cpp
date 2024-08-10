7025e05f9d8ba39e21b76728bbf12ec4223cf80f
Your Name <m.m.soleimani84@gmail.com>
2024-08-10 13:16:02 +0330

    Adding a comment to class Vexillary

--- Vexillary.cpp
+++ Vexillary.cpp
@@ -1,19 +1,24 @@
 #include "Vexillary.hpp"
 
+// Constructor to create an instance of the Vexillary
 Vexillary::Vexillary() : Card("vexillary", 0, "When a player plays a flag, that war is immediately over and that round of the game goes to the stage of determining the winner and then reviving.\n", Card::Color::Purple)
 {
     this->Set_Score(0);
 }
+
+// To handle the absence of errors due to overload and override
 std::string Vexillary::perform_Action() 
 {
     return this->Get_Type();
 }
 
+// To handle the absence of errors due to overload and override
 std::string Vexillary::perform_Action(std::shared_ptr<Player> & player) 
 {
     return this->Get_Type();
 }
 
+// Override and overloaded function to apply card abilities to the game
 std::string Vexillary::perform_Action(std::vector<std::shared_ptr<Player>> & players_turn) 
 {
     players_turn.clear();
--- Vexillary.hpp
+++ Vexillary.hpp
@@ -7,9 +7,9 @@ class Vexillary : public Card
 {
 public:
     Vexillary();                                                                         // Constructor to create an instance of the Vexillary
-    virtual std::string perform_Action() override;                                       // Override and overloaded function to apply card abilities to the game
+    virtual std::string perform_Action() override;                                       // To handle the absence of errors due to overload and override
     virtual std::string perform_Action(std::shared_ptr<Player> &) override;              // To handle the absence of errors due to overload and override
-    virtual std::string perform_Action(std::vector<std::shared_ptr<Player>> &) override; // To handle the absence of errors due to overload and override
+    virtual std::string perform_Action(std::vector<std::shared_ptr<Player>> &) override; // Override and overloaded function to apply card abilities to the game
 };
 
 #endif
